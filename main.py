09778efb3a27d4d58aa78cb74549b6161adb9026
akeyi2018 <akeyi2016@gmail.com>
2025-02-09 23:33:28 +0900

    feature img fit window size

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 import pyglet
 
+# 画像を読み込む
+img = pyglet.resource.image('fortress.png')
 
-window = pyglet.window.Window()
+# 画像のサイズに基づいてウィンドウのサイズを設定
+window = pyglet.window.Window(width=img.width, height=img.height)
 
 label = pyglet.text.Label('hello world pyglet..', 
                           font_name='Times New Roman', 
-                          font_size= 40,
-                          x = window.width / 2,
-                          y = window.height / 2,
+                          font_size=40,
+                          x=window.width // 2,
+                          y=window.height // 2,
                           anchor_x='center',
                           anchor_y='center')
 
 @window.event
 def on_draw():
     window.clear()
-    label.draw()
-
+    img.blit(x=0, y=0)  # 画像を描画
+    label.draw()  # ラベルを描画
 
 pyglet.app.run()
-
