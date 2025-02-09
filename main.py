9b58959a32cc23175439bd73c100ec20eaf5eb4c
akeyi2018 <akeyi2016@gmail.com>
2025-02-10 00:02:10 +0900

    fix display

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import pyglet
+from pyglet.window import Window
 
 # 画像を読み込む
 img = pyglet.resource.image('fortress.png')
 
 # 画像のサイズに基づいてウィンドウのサイズを設定
-window = pyglet.window.Window(width=img.width, height=img.height)
+window = Window(caption="Game", 
+                              width=img.width, 
+                              height=img.height, 
+                              style=Window.WINDOW_STYLE_TRANSPARENT)
+
+platform = pyglet.app.platform  # プラットフォーム情報を取得
+display = pyglet.display.get_display()
+screen = display.get_default_screen()
+
+# ウィンドウをスクリーンの中央に配置
+window_x = (screen.width - window.width) // 2
+window_y = (screen.height - window.height) // 2
+window.set_location(window_x, window_y)
 
 label = pyglet.text.Label('hello world pyglet..', 
                           font_name='Times New Roman', 
